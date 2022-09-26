e9594f9488924c7c7a23aef81963593fd8c0e960
dalenti <114300375+dalenti@users.noreply.github.com>
2022-09-26 12:20:11 +0700

    Add files via upload

---
+++ JavaPBOno1.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+public class JavaPBOno1 {
+     public static void main(String[]args){
+         
+       //No 1
+        
+        JOptionPane.showMessageDialog(null, "Belajar JAVA");
+        JOptionPane.showMessageDialog(null, "Sangat mudah sekali");
+        JOptionPane.showMessageDialog(null, "dan sangat menyenangkan");
+    
+    System.out.println("Belajar JAVA");
+    System.out.println("Sangat mudah sekali");
+    System.out.println("dan sangat menyenangkan");
+    
+       
+    }
+}
---
+++ JavaPBOno2.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+
+
+public class JavaPBOno2 {
+     public static void main(String[]args){
+     
+     //No 2
+        
+        String MasukanKata = JOptionPane.showInputDialog("Anda Sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar " + MasukanKata + " sangat mudah ");
+        
+    }
+}
